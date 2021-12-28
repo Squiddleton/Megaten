7b5940d0c33e5d913a0812ff6ec0d2275a42aca5
Squiddleton <mdlegomaster@gmail.com>
2021-12-27 23:49:48 -0500

    installed eslint

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const { personas, skills, treasureDemons } = require('./lists.json');
 
-module.exports = {
-	personas,
-	skills,
-	treasureDemons,
-};
+module.exports.personas = personas;
+module.exports.skills = skills;
+module.exports.treasureDemons = treasureDemons;
