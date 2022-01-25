82df1a27b5410728c391fd657100ec43af53f9e0
Squiddleton <mdlegomaster@gmail.com>
2022-01-25 18:06:58 -0500

    exports many classes at once

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const { demons, skills, treasureDemons } = require('./lists.json');
  * @returns {string} The input string without its punctuation
  */
 const noPunc = str => {
-	if (typeof str !== 'string') throw new TypeError('The noPunc function only accepts string inputs.');
-
 	return str
 		.toLowerCase()
 		.replace(/&/g, 'and')
@@ -17,41 +15,40 @@ const noPunc = str => {
 		.replace(/[^0-9a-z]/gi, '');
 };
 
-module.exports.Demon = Demon;
-
-module.exports.BaseSkill = BaseSkill;
-
-module.exports.AilBoostSkill = AilBoostSkill;
-module.exports.AilDefensiveSkill = AilDefensiveSkill;
-module.exports.AilmentSkill = AilmentSkill;
-module.exports.AttackSkill = AttackSkill;
-module.exports.AutoBuffSkill = AutoBuffSkill;
-module.exports.BarrierSkill = BarrierSkill;
-module.exports.BarrierBreakSkill = BarrierBreakSkill;
-module.exports.BoostSkill = BoostSkill;
-module.exports.BreakSkill = BreakSkill;
-module.exports.ChargeSkill = ChargeSkill;
-module.exports.CounterSkill = CounterSkill;
-module.exports.CritSkill = CritSkill;
-module.exports.CritBoostSkill = CritBoostSkill;
-module.exports.DefensiveSkill = DefensiveSkill;
-module.exports.DrainSkill = DrainSkill;
-module.exports.EndureSkill = EndureSkill;
-module.exports.EvasionSkill = EvasionSkill;
-module.exports.HalveSkill = HalveSkill;
-module.exports.InstaKillSkill = InstaKillSkill;
-module.exports.InstaKillBoostSkill = InstaKillBoostSkill;
-module.exports.MasterSkill = MasterSkill;
-module.exports.MiscSkill = MiscSkill;
-module.exports.NaviSkill = NaviSkill;
-module.exports.PostBattleSkill = PostBattleSkill;
-module.exports.RecoverySkill = RecoverySkill;
-module.exports.RegenSkill = RegenSkill;
-module.exports.SupportSkill = SupportSkill;
-module.exports.SusceptibilitySkill = SusceptibilitySkill;
-module.exports.WallSkill = WallSkill;
-
-module.exports.TreasureDemon = TreasureDemon;
+module.exports = {
+	Demon,
+	BaseSkill,
+	AilBoostSkill,
+	AilDefensiveSkill,
+	AilmentSkill,
+	AttackSkill,
+	AutoBuffSkill,
+	BarrierSkill,
+	BarrierBreakSkill,
+	BoostSkill,
+	BreakSkill,
+	ChargeSkill,
+	CounterSkill,
+	CritSkill,
+	CritBoostSkill,
+	DefensiveSkill,
+	DrainSkill,
+	EndureSkill,
+	EvasionSkill,
+	HalveSkill,
+	InstaKillSkill,
+	InstaKillBoostSkill,
+	MasterSkill,
+	MiscSkill,
+	NaviSkill,
+	PostBattleSkill,
+	RecoverySkill,
+	RegenSkill,
+	SupportSkill,
+	SusceptibilitySkill,
+	WallSkill,
+	TreasureDemon,
+};
 
 /** @type {import('./index').Demon[]} */
 module.exports.demons = demons.map(demon => new Demon(demon));
