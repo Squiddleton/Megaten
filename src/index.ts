10ff29e454e53010c462e719126c08859b4dcbb8
Squiddleton <mdlegomaster@gmail.com>
2022-11-20 20:51:38 -0500

    Sort exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
+export * from './dataTypes';
 export * from './demon';
 export { default as MegatenError } from './error';
-export * from './dataTypes';
 export * from './skill';
 export * from './types';
