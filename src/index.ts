da99892af0eb25cbfde9b0689849c47659998706
Squiddleton <mdlegomaster@gmail.com>
2022-12-09 14:19:14 -0500

    Add battle themes for completed games

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './dataTypes';
 export * from './demon';
 export * from './error';
+export * from './music';
 export * from './skill';
 export * from './types';
---
+++ src/music.ts
@@ -0,0 +1,89 @@
+/** Megaten battle themes */
+export namespace BattleThemes {
+	/** Battle themes from Shin Megami Tensei V */
+	export enum ShinMegamiTensei5 {
+		/** Plays during the first battle against Daemon and against many bosses, such as Nuwa */
+		HumansDemonsAnd = 'https://open.spotify.com/track/1bHtqacPHldcny8gKLgUoe?si=4dd68a5bfad142fb',
+		/** Plays during normal battles */
+		Daat = 'https://open.spotify.com/track/6qZ2JimnPvcBRyW9Aa9A97?si=d25a4e9c271e4e33',
+		/** Plays against Abcesses */
+		Magatsuka = 'https://open.spotify.com/track/5pbb01JsiBUp5mGS8BGTdz?si=8eec937768a84f1f',
+		/** Plays against many bosses, such as Hydra */
+		Ferocity = 'https://open.spotify.com/track/785siCQxHOCRemGJxVw6Li?si=91202cccbaf84fd7',
+		/** Plays against some quest bosses, such as Pazuzu */
+		ClashOfTheMighty = 'https://open.spotify.com/track/6oAVeG06IaMFK4mHTKBdvR?si=eb30e2e85e864a40',
+		/** Plays against many chaos-ailgned bosses, such as Eligor */
+		Strength = 'https://open.spotify.com/track/270TvB2x0BK8WJAcWNf5uw?si=9bf126fbf8b54a0b',
+		/** Plays against some quest bosses, such as Oni */
+		Droll = 'https://open.spotify.com/track/4eJYu17otQD83MPVOVkuIy?si=824f215502ec4633',
+		/** Plays against many bosses, such as Fionn mac Cumhaill */
+		Origin = 'https://open.spotify.com/track/5qw0jgyeHirCO6UiPPGrmh?si=70d488271e8d4cc5',
+		/** Plays against some quest bosses, such as Anahita */
+		Seeker = 'https://open.spotify.com/track/1ZXtpcMMi8G5kR7oie7Zse?si=cfb87d39884641b7',
+		/** Plays against some bosses, such as Ishtar */
+		DancingCrazyMurder = 'https://open.spotify.com/track/2626lsAIrawbGUBovDZZ7Q?si=4a53cc1b749c4ff8',
+		/** Plays during normal battles in dungeons */
+		Edifice = 'https://open.spotify.com/track/1JqsoHAAtIhaBYFa5wIUCX?si=d489f271c046421c',
+		/** Plays against Mitamas */
+		Mitama = 'https://open.spotify.com/track/40l360SI8nd3u1qUFqsbce?si=a7ea9bfb8f534875',
+		/** Plays against Fury Shiva */
+		Destruction = 'https://open.spotify.com/track/2CmTSrnpy8TBl3fdvcjY6P?si=d5bd93a3a0c84778',
+		/** Plays against many law-aligned bosses, such as Camael */
+		Seraph = 'https://open.spotify.com/track/3dCCF4hL3wFvYEkRrpJPXb?si=d3b218ffd826472c',
+		/** Plays against Nahobino Abdiel */
+		Abdiel = 'https://open.spotify.com/track/6OqE3lLzo3uyM5sKW6OMDA?si=627dc0413368497b',
+		/** Plays against Nahobino Tsukuyomi */
+		Tsukuyomi = 'https://open.spotify.com/track/2FNgjWRJmPUbzmKZku882o?si=ce8767599df84f5f',
+		/** Plays against Nahobino Abdiel */
+		Nuwa = 'https://open.spotify.com/track/7izJxuR9bvbvS2zBIXji1X?si=0fe95a22964840e8',
+		/** Plays against Matter Lucifer */
+		Eon = 'https://open.spotify.com/track/4jDJy7X1KaQNibOQT6OS2b?si=179542e826f5471a',
+		/** Plays against DLC bosses, such as Cleopatra */
+		Addition = 'https://open.spotify.com/track/1Ebj9boU3nrfLp0wOyNJxj?si=ca12c70772044a2f',
+		/** Plays against Fiends */
+		Fiend = 'https://open.spotify.com/track/5I6kKTcEq79FWvKM8Fq88h?si=59483912833f4e3e',
+		/** Plays against King Demi-fiend */
+		DemiFiend = 'https://open.spotify.com/track/3QnNIehRq1iR5PxiEQwmUB?si=a285cfd2792d4adb'
+	}
+	/** Battle themes from Persona 5, Persona 5 Royal, and Persona 5 Strikers */
+	export enum Persona5 {
+		/** Plays during awakening battles */
+		WillPower = 'https://open.spotify.com/track/7eA7zqW045T9BfzcC90NZl?si=265bd99fd76a47b0',
+		/** Plays during normal battles */
+		LastSurprise = 'https://open.spotify.com/track/4cPnNnTMkJ6soUOUzEtmcp?si=01eee74b90ab45a9',
+		/** Plays against Palace rulers */
+		BloomingVillain = 'https://open.spotify.com/track/60uFWIxiY4gik3WbC5DeSu?si=c26a3049e9584c28',
+		/** Plays against mini-bosses, such as Archangel */
+		KeeperOfLust = 'https://open.spotify.com/track/1DHmohOmdcn1YJKlTV4HHp?si=13d7be6030454c17',
+		/** Plays against some late-game bosses, such as Shadow Shido */
+		RiversInTheDesert = 'https://open.spotify.com/track/6rgIKCKKCnhyaNTgmY73tq?si=c298823e3be64931',
+		/** Plays against Yaldabaoth */
+		Jaldabaoth = 'https://open.spotify.com/track/3zihQwHK8BwkufVUjKXUP7?si=2fa29061476548d3',
+		/** Plays during ambushes in Royal */
+		TakeOver = 'https://open.spotify.com/track/1iNKgHv2k6cB09aB5ozR55?si=43dfd949094144a9',
+		/** Plays during Challenge Battles */
+		PrisonLabor = 'https://open.spotify.com/track/6g3CfpQKSDG2QFPStFme1l?si=15c57bc6b0a9444e',
+		/** Plays against Azathoth */
+		KeepYourFaith = 'https://open.spotify.com/track/2f3mQr2w8DQhAb8X3zB1pZ?si=eaf82a01afdb48c7',
+		/** Plays against Adam Kadmon */
+		ThrowAwayYourMask = 'https://open.spotify.com/track/2uNrsZLJloillM1kbzxgmn?si=48711f5f84a743bd',
+		/** Plays during many normal battles in Strikers */
+		WhatYouWishFor = 'https://open.spotify.com/track/1cVqfZ3r5xsviAe6HKaFGc?si=696c6f77126c4fcb',
+		/** Plays against Monarchs in Strikers */
+		BloomingVillainScramble = 'https://open.spotify.com/track/7FnHsghGxOhBsbXJ5ci7uh?si=7c927841a29a4dd1',
+		/** Plays during many normal battles in Strikers */
+		AxeToGrind = 'https://open.spotify.com/track/6uUYdIpODNtc7QbWDLRCeK?si=317fc00ddb6543d7',
+		/** Plays against mini-bosses, such as Lock Keepers */
+		KeeperOfLustScramble = 'https://open.spotify.com/track/0XADk0lLZU0Cwr2boRVWL0?si=5e7f62a914124609',
+		/** Plays during some normal battles and against Akane's Joker */
+		LastSurpriseScramble = 'https://open.spotify.com/track/2CMvsKTiEIJ0jI3oAniDe2?si=83cda8f707d24d7a',
+		/** Plays against some late-game bosses in Strikers, such as Zephyrus Mech */
+		RiversInTheDesertScramble = 'https://open.spotify.com/track/1uNgekMKe3a7JNqiFakbcb?si=15f84ca2183c4e68',
+		/** Plays against Akira the Hero and at Tokyo Tower */
+		CounterStrike = 'https://open.spotify.com/track/3WvvYdNIcRBh8pzvZ03khk?si=26851e67ab1a4c25',
+		/** Plays against False God Demiurge's first stage */
+		DemiurgeIExodus = 'https://open.spotify.com/track/6tGRgonQSLvG1CCS8GBsIt?si=493217adfa9947e9',
+		/** Plays against False God Demiurge's second stage */
+		DemiurgeIINegai = 'https://open.spotify.com/track/7cXMQJaeGFTVuRYjrx7nZj?si=11f35477730d447c'
+	}
+}
