ba7f5f2a93db3d5e684ddc4a9fe6c50e641bee44
Squiddleton <mothmain9515@outlook.com>
2023-02-07 23:48:02 -0500

    Add missing classes to README

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
+import { BattleThemes } from './music';
+
 export * from './dataTypes';
 export * from './demon';
 export * from './error';
 export * from './music';
 export * from './skill';
 export * from './types';
+
+BattleThemes.Persona5.LastSurprise;
