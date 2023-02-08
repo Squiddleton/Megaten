b34eb247640776314eef6710e6694a298f3d4409
Squiddleton <mothmain9515@outlook.com>
2023-02-07 22:39:42 -0500

    Remove tests

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,6 @@
-import { Skill } from './skill';
-
 export * from './dataTypes';
 export * from './demon';
 export * from './error';
 export * from './music';
 export * from './skill';
 export * from './types';
-
-const s = Skill.get('agi', true);
-if (s.type === 'ATTACK') {
-	s.power.display;
-}
